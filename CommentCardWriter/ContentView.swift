21f7e3011fdfb5c862e36877cee78d132d5e64c6
Luke Vereker <vereker.l@etoncollege.org.uk>
2023-02-04 11:19:03 +0000

    Generate comment button

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Text("Comment Card Generator")
+            Button("Generate Comment", action: {
+                print("")
+            })
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
